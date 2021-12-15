c8f276f19354e43c240e9977a5fabf7066f48499
oguz <oguzkahraman001@gmail.com>
2021-12-15 18:11:07 +0300

    new

--- App.js
+++ App.js
@@ -1,8 +1,9 @@
 
 import React from 'react';
-import {Text, TouchableOpacity, View } from 'react-native';
+import {Text, TouchableOpacity, View,Image } from 'react-native';
 import { Actions, Router, Scene } from 'react-native-router-flux';
 import Inputs from './Inputs';
+import { Matches } from './Matches';
 
 
 const Home = () => {
@@ -12,17 +13,24 @@ const Home = () => {
   const goToForm = () => {
     Actions.form();
   };
+  const goToMatches = () => {
+    Actions.matches();
+  };
 
 
   return(
 <View>
       <TouchableOpacity style={{margin: 128}} onPress={goToAbout}>
-      <Text>This is HOME!</Text>
-      </TouchableOpacity>,
+      <Text>This is About!</Text>
+      <Image source={require('./img/goz.png')} />
+      </TouchableOpacity>
       <TouchableOpacity style={{margin: 128}} onPress={goToForm}>
       <Text>This is Form!</Text>
-      </TouchableOpacity>,
-      
+      <Image source={require('./img/goz.png')} />
+      </TouchableOpacity>
+      <TouchableOpacity style={{margin: 128}} onPress={goToMatches}>
+      <Text>This is Matches</Text>
+      </TouchableOpacity>
       </View>
   );
 };
@@ -33,9 +41,13 @@ const Form = () => {
   return(
     <View>
       <TouchableOpacity style={{margin: 128}} onPress={goToHome}>
-      <Text>This is ABOUT!</Text>
+      <Text>This is Home!</Text>
     </TouchableOpacity>
     <Inputs/>
+    <Image source={require('./img/tablo.jpg')} 
+      style={{ width: 200, height: 150 }}
+    />
+  
   </View>
   );
 };
@@ -51,11 +63,14 @@ const About = () => {
   return(
     <View>
     <TouchableOpacity style={{margin: 128}} onPress={goToHome}>
-      <Text>This is ABOUT!</Text>
+      <Text>This is Home!</Text>
+    
     </TouchableOpacity>
     <TouchableOpacity style={{margin: 128}} onPress={goToForm}>
     <Text>This is Form!</Text>
   </TouchableOpacity>
+  
+
   </View>
   );
 };
@@ -67,6 +82,7 @@ const App = () => {
       <Scene key="home" component={Home} title="Home" initial={true}></Scene>
       <Scene key="about" component={About} title="About"></Scene>
       <Scene key="form" component={Form} title="Form"></Scene>
+      <Scene key="matches" component={Matches} title="Matches"></Scene>
     </Scene>
   </Router>
   )
---
+++ Matches.js
@@ -0,0 +1,42 @@
+import React, { useEffect, useState } from 'react';
+import {Text,  View,StyleSheet,Picker} from 'react-native';
+
+export const Matches = () => {
+  const [User,setUser] =useState("");
+  const [SelectedYear,setSelectedYear] =useState(2011);
+  const [totalMatches,settotalMatches] =useState(0);
+  const [result,setResult]=useState([]);
+
+ 
+  const UpdateUser = (User) => {
+    setUser(User);
+  };
+
+  useEffect(() => {
+        fetch(`https://jsonmock.hackerrank.com/api/football_competitions?year=$(SelectedYear)`
+
+        )
+        .then((response)=>response.json())
+        .then((result))
+
+  })
+  return (
+    <View>
+       <Picker selectedValue = {setUser} onValueChange = {UpdateUser}>
+          <Picker.Item label = "Steve" value = "steve" />
+          <Picker.Item label = "Ellen" value = "ellen" />
+          <Picker.Item label = "Maria" value = "maria" />
+       </Picker>
+       <Text style = {styles.text}>{setUser}</Text>
+    </View>
+ );
+  };
+export default Matches
+
+const styles = StyleSheet.create({
+   text: {
+      fontSize: 30,
+      alignSelf: 'center',
+      color: 'red'
+   }
+});
